ee4813709246f1bcc62ab2ea67fe98942bd6e0d6
kalukkapippula <62897693+kalukkapippula@users.noreply.github.com>
2020-06-22 10:38:14 +0200

    Add files via upload

---
+++ lezione 3/15.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+	printf("13_Prodotto somme successive\n");
+	unsigned num1=0;
+	unsigned num2=0;
+	unsigned prodotto=0;
+	scanf("%u", &num1);
+	scanf("%u", &num2);
+	for(;num2>=1;num2--){
+		prodotto+=num1;
+		printf("\n%u", prodotto);
+	}
+	return 0;
+}
